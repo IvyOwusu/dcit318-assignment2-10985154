2b54d22b8fa5a8001fda1ec2fa149af440aab650
unknown <iowusu033@st.ug.edu.gh>
2024-06-15 19:14:55 +0000

    Implemented Inheritance & Overriding in a C# App

---
+++ AnimalSounds/Program.cs
@@ -0,0 +1,49 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace AnimalSounds
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class Cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Animal, Dog, and Cat
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Call the MakeSound() method on each instance
+            genericAnimal.MakeSound(); // Output: Some generic sound
+            dog.MakeSound(); // Output: Bark
+            cat.MakeSound(); // Output: Meow
+        }
+    }
+}
